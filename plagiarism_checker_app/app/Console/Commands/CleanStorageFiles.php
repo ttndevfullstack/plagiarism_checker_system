11ec434a975c3f93363b1fea2239b3005a7b9a2f
Minh Nguyen <minh.nguyen.ngoc@tpssoft.com>
2025-06-12 17:38:02 +0700

    feat: add clean storage commands

---
+++ plagiarism_checker_app/app/Console/Commands/CleanStorageFiles.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+class CleanStorageFiles extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:clean-storage-files';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $paths = [
+            storage_path('app/livewire-tmp'),
+            storage_path('app/public/downloads'),
+        ];
+
+        foreach ($paths as $path) {
+            if (is_dir($path)) {
+                // Delete all files
+                foreach (File::files($path) as $file) {
+                    File::delete($file);
+                }
+                // Delete all subdirectories
+                foreach (File::directories($path) as $dir) {
+                    File::deleteDirectory($dir);
+                }
+            }
+        }
+
+        $this->info('Cleaned files and folders in: livewire-tmp, downloads.');
+    }
+}
---
+++ plagiarism_checker_app/app/Console/Commands/ClearStorageFiles.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+class ClearStorageFiles extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:clear-storage-files';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $paths = [
+            storage_path('app/livewire-tmp'),
+            storage_path('app/public/downloads'),
+            storage_path('app/public/media'),
+        ];
+
+        foreach ($paths as $path) {
+            if (is_dir($path)) {
+                // Delete all files
+                foreach (File::files($path) as $file) {
+                    File::delete($file);
+                }
+                // Delete all subdirectories
+                foreach (File::directories($path) as $dir) {
+                    File::deleteDirectory($dir);
+                }
+            }
+        }
+
+        $this->info('Cleared files and folders in: livewire-tmp, downloads, media.');
+    }
+}
